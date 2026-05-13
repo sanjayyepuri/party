fbc59325dc7dec88c74366e82d82948569cc76a6
Sanjay Yepuri <sanjay.yepuri@gmail.com>
2026-05-12 21:11:25 -0500

    cleanup intro

--- app/bouncer/app/page.tsx
+++ app/bouncer/app/page.tsx
@@ -7,20 +7,14 @@ export default async function HomePage() {
         a space for my friends to stay in touch.
       </h1>
       <p className="mb-5">
-        My friend list on social media has become heavily diluted. This means
-        there are many people that I have never spent time with -- folks I had
-        met once and never had the time or opportunity to reach out again.
-      </p>
-      <p className="mb-5">
         I have an amazing community of friends who I do spend a meaningful
         amount of time with. It&apos;s these people I want to create a space for
         -- a place where the noise of media and all of its distractions
         disappears.
       </p>
       <p className="mb-5">
-        This community will be open. It&apos;s
-        not an invitation to reach out. It&apos;s an automatic invitation to
-        come through.
+        This community will be open. It&apos;s not an invitation to reach out.
+        It&apos;s an automatic invitation to come through.
       </p>
       <div className="mt-8">
         <LoginButton />
